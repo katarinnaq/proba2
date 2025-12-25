1aa14c1a28d436b4fbf3f9c1eeb731240d1acb4e
katarinnaq <kpetrovic2523it@raf.rs>
2025-12-25 11:13:34 +0100

    Fajl nakon dodavanja rola i kreiranja admin usera

--- database/migrations/2025_12_25_100206_add_role_to_users_table.php
+++ database/migrations/2025_12_25_100206_add_role_to_users_table.php
@@ -9,20 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+   public function up(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->string('role')->default('kupac'); 
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+public function down(): void
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn('role');
+    });
+}
+
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,16 +11,19 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        
+        User::create([
+            'name' => 'Ana Krasić',
+            'email' => 'ana@eterna.rs',
+            'password' => Hash::make('lozinka123'), 
+            'role' => 'admin',
         ]);
     }
 }
